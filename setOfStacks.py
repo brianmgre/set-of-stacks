88a9472e889e094046856b0124efc3b452279ee9
Brian G <brian.gregorius@gmail.com>
2019-02-21 15:44:28 -0500

    inital commit

---
+++ setOfStacks.py
@@ -0,0 +1,29 @@
+
+class SetOfStacks:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.stacks = [[]]
+        self.length = 0
+
+    def push(self, item):
+        if self.stacks[self.length] < self.capacity:
+
+    def add(self):
+        self.length += 1
+
+    def subtract(self):
+        self.length -= 1
+
+
+stacks = SetOfStacks(3)
+
+stacks.push(10)
+stacks.push(9)
+stacks.push(8)
+stacks.push(7)
+
+print(stacks.length)
+print(stacks.popAt(0))
+print(stacks.pop())
+print(stacks.pop())
+print(stacks.length)
